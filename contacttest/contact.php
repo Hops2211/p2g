2448b2d4942bee23a9bb193f10e3d3c18e5c7b36
Hops2211 <rjohns42@hawk.iit.edu>
2017-05-25 14:30:18 -0400

    trying php

--- contacttest/contact.php
+++ contacttest/contact.php
@@ -2,7 +2,7 @@
 <?php
 
 if($_POST["submit"]) {
-    $recipient="your@email.address";
+    $recipient="prepd2go@gmail.com";
     $subject="Form to email message";
     $sender=$_POST["sender"];
     $senderEmail=$_POST["senderEmail"];
